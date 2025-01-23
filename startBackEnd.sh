51ec49f78dbde3cb36fa05d37c7caeee9aa364fc
Jackson Codd <coddjackson@gmail.com>
2025-01-22 21:59:54 -0500

    This fork allows users to pick which ai they want to run locally
    they can pick an ai and set a description for it to chat with any open source model on ollama

--- startBackEnd.sh
+++
@@ -1,5 +0,0 @@
-cd /Users/jcodd/Downloads
-eval "$(brew/bin/brew shellenv)"     
-brew services start mongodb-community@8.0
-cd /Users/jcodd/Documents/GitHub/AISpanish/SpanishAIAssitant/backend
-node server.js
